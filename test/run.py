7f2185f2ed2abf81734d7f6d3470764ec6e3a4d2
Lin Han <me@linhan.email>
2022-05-03 13:38:51 -0400

    save

--- test/run.py
+++ test/run.py
@@ -11,11 +11,11 @@ config_name=$config_name
 yml=lung_coronavirus/${config_name}
 save_dir_all=saved_model
 save_dir=saved_model/${config_name}
-mkdir -p $save_dir
+mkdir -p ${save_dir}
 
 # Train the model: see the train.py for detailed explanation on script args
 python3 train.py --config configs/${yml}.yml \
---save_dir $save_dir \
+--save_dir ${save_dir} \
 --save_interval 5 --log_iters 1 \
 --num_workers 2 --do_eval --use_vdl \
 --keep_checkpoint_max 5  --seed 0  
